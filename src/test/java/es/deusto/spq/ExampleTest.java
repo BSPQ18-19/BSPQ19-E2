66b1bf2c726568d2143830b9bf7de6a6556262c0
iapellaniz <16291012+iapellaniz@users.noreply.github.com>
2019-03-30 12:16:30 +0100

    Create example test

---
+++ src/test/java/es/deusto/spq/ExampleTest.java
@@ -0,0 +1,14 @@
+package es.deusto.spq;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void testingExample()
+    {
+        Assert.assertTrue(true);
+    }
+
+}
