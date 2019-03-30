dab8c27ee4be0d55a06f035888ba4c82f4eaab3e
iker-barriocanal <ikerbl98@gmail.com>
2019-03-30 14:22:07 +0100

    User data model
    Created abstract superclass User and Guest class, with JDO annotations.

---
+++ src/main/java/es/deusto/spq/server/data/dao/Guest.java
@@ -0,0 +1,39 @@
+package es.deusto.spq.server.data.dao;
+
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
+public class Guest extends User {
+
+	private int phone;
+	private String address;
+	
+	public Guest(int userID, String name, String email, String password, int phone, String address) {
+		super(userID, name, email, password, phone, address);
+		this.phone = phone;
+		this.address = address;
+	}
+	
+	public int getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [ID=" + this.getUserID() + ", name=" + this.getName() + ", email:" + this.getEmail() + ", phone="
+				+ phone + ", address=" + address + "]";
+	}
+
+}
