e8d9924fd75a703278bde5e87527dcc13b7d0d61
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2019-04-02 23:15:53 +0200

    Documentation of IUserDAO

--- src/main/java/es/deusto/spq/server/data/dao/IUserDAO.java
+++ src/main/java/es/deusto/spq/server/data/dao/IUserDAO.java
@@ -6,8 +6,19 @@ import es.deusto.spq.server.data.jdo.User;
 
 public interface IUserDAO {
 
+	/**Retrieves all the users in the data base.
+	 * @return a list of all the users.
+	 */
 	public List<User> getUsers();
+	/**Retrieves the user that matches the ID.
+	 * @param ID the ID of the user to retrieve.
+	 * @return the user matching the ID if exists, and null if not.
+	 */
 	public User getUserbyID(String ID);
+	/**Creates the user in the data base, and returns a detached copy.
+	 * @param user the new user to be stored.
+	 * @return a detached copy of the new user, and null if an error has arised.
+	 */
 	public User createUser(User user);
 	
 }
