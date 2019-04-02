2049e834c2ca6dada9b3b624d07bccafe6ad01d7
Egoitz <egoes98@gmail.com>
2019-04-02 17:52:49 +0200

    The DB Test like the one he has done
    This Junit is a DB test similar to the one he has done in the examples. I will do one calling the DAO methods, to see what do you prefer.

---
+++ src/test/java/es/deusto/spq/TestDB.java
@@ -0,0 +1,89 @@
+package es.deusto.spq;
+
+import static org.junit.Assert.*;
+import java.util.Iterator;
+import java.util.List;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Extent;
+import javax.jdo.Query;
+import javax.jdo.JDOHelper;
+import javax.jdo.Transaction;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import es.deusto.spq.server.data.jdo.*;
+import es.deusto.spq.server.data.MyPersistenceManager;
+
+public class TestDB {
+	private PersistenceManagerFactory pmf;
+	private PersistenceManager pm;
+	private Transaction tx = null;
+	
+	@Before
+	public void setUp() throws Exception{
+        System.out.println("DataNucleus AccessPlatform with JDO");
+        System.out.println("===================================");
+
+        this.pm = MyPersistenceManager.getPersistenceManager();
+        this.tx = this.pm.currentTransaction();
+	}
+	
+
+	@Test
+	public void test() {
+		try {
+			tx.begin();
+			//Saving user
+			System.out.println("Persisting users");
+			Guest guest = new Guest("0", "Test", "email@email.email", "Password", 11223344, "Test Street");
+			System.out.println("> " + guest);
+			pm.makePersistent(guest);
+			System.out.println("");
+			
+			//User Extend
+			System.out.println("Retrieving Extent for Users");
+            Extent<Guest> e = pm.getExtent(Guest.class, true);
+            Iterator<Guest> iter = e.iterator();
+            if(iter.hasNext()) {
+           	 Object obj = iter.next();
+           	 System.out.println("> " +obj);
+            }
+            System.out.println("");
+            
+            //User Query
+            System.out.println("Executing Query for getting the user 'Test'");
+			Query<Guest> q = pm.newQuery(e,"name=='Test'");
+			for(Guest g : (List<Guest>) q.execute()) {
+				System.out.println("> " +g);
+			}
+			System.out.println("");
+			
+			//User deletion
+			System.out.println("Delete the Test user");
+            long deletedUsers = q.deletePersistentAll();
+            System.out.println("Deleted Users: " + deletedUsers);
+            System.out.println("");
+            
+			tx.commit();
+		}catch(Exception e) {
+        	System.out.println("Exception thrown during retrieval of Extent : " + e.getMessage());
+		}finally {
+			 if (tx.isActive())
+	            {
+	                tx.rollback();
+	            }
+	            pm.close();
+	    }
+		System.out.println("");
+	}
+	
+	@After
+    public void tearDown() throws Exception {
+		
+        if (this.pm != null) {
+			this.pm.close();
+		}
+		
+    }
+}
