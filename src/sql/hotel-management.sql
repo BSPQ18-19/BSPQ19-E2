3ed385ae160ed2e5eab37c48313889b7db57424c
Egoitz <egoes98@gmail.com>
2019-04-02 13:40:21 +0200

    DB SQL file

--- src/sql/hotel-management.sql
+++ src/sql/hotel-management.sql
@@ -1,1 +1,11 @@
-/* Insert SQL script here */
+/* DELETE 'nucleus' database*/
+DROP SCHEMA IF EXISTS hotelManagementDB;
+/* DELETE USER 'hm' AT LOCAL SERVER*/
+DROP USER IF EXISTS 'hm'@'%';
+
+/* CREATE 'nucleus' DATABASE */
+CREATE SCHEMA hotelManagementDB;
+/* CREATE THE USER 'hm' AT LOCAL SERVER WITH PASSWORD 'hm' */
+CREATE USER IF NOT EXISTS 'hm'@'%' IDENTIFIED BY 'hm';
+
+GRANT ALL ON hotelManagementDB.* TO 'hm'@'%';
