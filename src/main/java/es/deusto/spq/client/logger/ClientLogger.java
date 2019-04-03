394b384fda11d78b5d24c5f337647483cc22b803
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2019-04-03 16:45:03 +0200

    Added output files for logs

--- src/main/java/es/deusto/spq/client/logger/ClientLogger.java
+++ src/main/java/es/deusto/spq/client/logger/ClientLogger.java
@@ -1,6 +1,8 @@
 package es.deusto.spq.client.logger;
 
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**The logger manager for the client, so that there's only one log in the client.
  * It can be retrieved to log messages.
@@ -13,9 +15,18 @@ public class ClientLogger {
 	 * 
 	 */
 	private static Logger log;
+	private static FileHandler fh;
 	
 	static {
 		log = Logger.getLogger(ClientLogger.class.getName());
+		try {
+			fh = new FileHandler("log/ClientLog.log");
+			log.addHandler(fh);
+			fh.setFormatter(new SimpleFormatter());
+		} catch (Exception e) {
+			log.severe("Cannot set the file handler for the logger. " + e.getMessage());
+		}
+//		log.info("test");
 	}
 	
 	/**Retrieves the logger to be used in the client.
--- src/main/java/es/deusto/spq/server/logger/ServerLogger.java
+++ src/main/java/es/deusto/spq/server/logger/ServerLogger.java
@@ -1,6 +1,8 @@
 package es.deusto.spq.server.logger;
 
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**The logger manager for the server, so that there's only one log in the server.
  * It can be retrieved to log messages.
@@ -13,9 +15,17 @@ public class ServerLogger {
 	 * 
 	 */
 	private static Logger log;
+	private static FileHandler fh;
 	
 	static {
 		log = Logger.getLogger(ServerLogger.class.getName());
+		try {
+			fh = new FileHandler("log/ServerLog.log");
+			log.addHandler(fh);
+			fh.setFormatter(new SimpleFormatter());
+		} catch (Exception e) {
+			log.severe("Cannot set the file handler for the logger. " + e.getMessage());
+		}
 	}
 	
 	/**Retrieves the logger to be used in the server.
