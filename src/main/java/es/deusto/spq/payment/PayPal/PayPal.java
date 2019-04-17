61ce2640c175b811176b5d5c3f1328f0540c232a
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2019-04-17 20:21:14 +0200

    PayPal's server initial files
    Added a main class and Logger class in the PayPal external service.
    Modified pom.xml so that now there's a profile to run this server.
    log4j.properties file updated to support both console and file output
    for the logger.

---
+++ src/main/java/es/deusto/spq/payment/PayPal/PayPal.java
@@ -0,0 +1,10 @@
+package es.deusto.spq.payment.PayPal;
+
+import es.deusto.spq.payment.PayPal.logger.PayPalLogger;
+
+public class PayPal {
+
+	public static void main(String [] args) {
+		PayPalLogger.getLogger().info("Hello world!");
+	}
+}
