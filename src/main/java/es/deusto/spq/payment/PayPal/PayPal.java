a6dadaa0e0b1c8440ea0aaf71891565cd467caf9
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2019-04-21 14:52:40 +0200

    PayPal main class updated
    The main class has been updated:
    - Keeps track of the current executing threads in the PayPal server.
    - Implements a mutual exclusion access to the pool where threads
    mentioned in the previous point are stored.
    - Initializes the PayPal full server.

--- src/main/java/es/deusto/spq/payment/PayPal/PayPal.java
+++ src/main/java/es/deusto/spq/payment/PayPal/PayPal.java
@@ -1,10 +1,94 @@
 package es.deusto.spq.payment.PayPal;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.log4j.Logger;
+
+import es.deusto.spq.payment.PayPal.connections.Payer;
+import es.deusto.spq.payment.PayPal.connections.Registrator;
+import es.deusto.spq.payment.PayPal.connections.ServerListener;
 import es.deusto.spq.payment.PayPal.logger.PayPalLogger;
 
 public class PayPal {
 
+	private Logger log;
+	
 	public static void main(String [] args) {
-		PayPalLogger.getLogger().info("Hello world!");
+		int port = Integer.parseInt(args[0]);
+		PayPal server = new PayPal();
+		server.log = PayPalLogger.getLogger();
+		try {
+			server.start(port);
+		} catch (IOException e) {
+			server.log.fatal("Could not initialize PayPal server - " + e.getMessage());
+		}
 	}
+	
+	public PayPal() {
+		activeListeners = new ArrayList<ServerListener>();
+		activePayers = new ArrayList<Payer>();
+		activeRegistrators = new ArrayList<Registrator>();
+	}
+	
+	private static ReentrantLock listenerLock = new ReentrantLock();
+	private static List<ServerListener> activeListeners;
+	private static ReentrantLock payerLock = new ReentrantLock();
+	private static List<Payer> activePayers;
+	private static ReentrantLock registratorLock = new ReentrantLock();
+	private static List<Registrator> activeRegistrators;
+	
+	public static void addListener(ServerListener listener) {
+		listenerLock.lock();
+		activeListeners.add(listener);
+		listenerLock.unlock();
+	}
+	
+	public static void removeListener(ServerListener listener) {
+		listenerLock.lock();
+		activeListeners.remove(listener);
+		listenerLock.unlock();
+	}
+	
+	public static void addPayer(Payer payer) {
+		payerLock.lock();
+		activePayers.add(payer);
+		payerLock.unlock();
+	}
+	
+	public static void removePayer(Payer payer) {
+		payerLock.lock();
+		activePayers.add(payer);
+		payerLock.unlock();
+	}
+	
+	public static void addRegistrator(Registrator registrator) {
+		registratorLock.lock();
+		activeRegistrators.add(registrator);
+		registratorLock.unlock();
+	}
+	
+	public static void removeRegistrator(Registrator registrator) {
+		registratorLock.lock();
+		activeRegistrators.add(registrator);
+		registratorLock.unlock();
+	}
+	
+	public void start(int port) throws IOException {
+		ServerListener initialListener = new ServerListener(port);
+		activeListeners.add(initialListener);
+		initialListener.start();
+	}
+	
+	public static void closeServer() {
+		for(ServerListener serverListener : activeListeners)
+			serverListener.closeListener();
+		for(Payer payer : activePayers)
+			payer.closePayer();
+		for(Registrator registrator : activeRegistrators)
+			registrator.closeRegistrator();
+	}
+	
 }
