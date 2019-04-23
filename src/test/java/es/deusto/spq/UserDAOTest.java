5cbd8d2aa32590d6f7e7cbbcb4d455b476eb478d
Egoitz <egoes98@gmail.com>
2019-04-23 18:02:29 +0200

    Test
    Added a test method in the UserDAOTest that test the editUserMethod. Is not completly working becaus esome changes has to be made on the method in the DAO.

--- src/test/java/es/deusto/spq/UserDAOTest.java
+++ src/test/java/es/deusto/spq/UserDAOTest.java
@@ -10,6 +10,8 @@ import org.junit.runners.MethodSorters;
 import org.junit.Test;
 
 import es.deusto.spq.server.data.dao.UserDAO;
+import es.deusto.spq.server.data.dto.Assembler;
+import es.deusto.spq.server.data.dto.UserDTO;
 import es.deusto.spq.server.data.jdo.Guest;
 import es.deusto.spq.server.data.jdo.User;
 import junit.framework.Assert;
@@ -31,12 +33,14 @@ public class UserDAOTest {
 	private static UserDAO userDao;
 	private static User user;
 	private static String userID;
+	private static Assembler assembler;
 	
 	@BeforeClass
 	public static void initialize() {
 		userDao = new UserDAO();
+		assembler = new Assembler();
 		userID = "1";
-//		user = new Guest(userID, "name", "email", "password", 1, "address");
+		user = new Guest(userID, "name", "email", "password", "1", "address");
 		//TODO initialize user correctly
 	}
 	
@@ -66,7 +70,24 @@ public class UserDAOTest {
 	
 	@SuppressWarnings("deprecation")
 	@Test
-	public void dDeleteUserByID() {
+	public void dEditUser() {
+		String name = "name2", email = "email2", password = "password2", address =  "address2", phone = "phone2";
+		UserDTO actualUser = assembler.assembleUser(user);
+		//Apply the changes to the actual userDTO to compare it to the one DB gets.
+		actualUser.setName(name);
+		actualUser.setEmail(email);
+		actualUser.setPassword(password);
+		actualUser.setAddress(address);
+		actualUser.setPhone(phone);
+		
+		UserDTO changedUser = userDao.editUser(userID, name, email, password, phone, address);
+		Assert.assertEquals(actualUser, changedUser);
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void eDeleteUserByID() {
 //		Assert.assertTrue(userDao.deleteUserbyID(userID));
 		// TODO update this test
 	}
