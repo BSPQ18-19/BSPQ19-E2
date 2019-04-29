f7cd65ca98bf9a2c1ac1e7a7e8ee7a95a316ef58
Egoitz <egoes98@gmail.com>
2019-04-29 20:50:19 +0200

    Documentation on HotelDAO
    Added the missing Documentation on HotelDAO

--- src/main/java/es/deusto/spq/server/data/dao/IHotelDAO.java
+++ src/main/java/es/deusto/spq/server/data/dao/IHotelDAO.java
@@ -6,9 +6,35 @@ import es.deusto.spq.server.data.jdo.Hotel;
 
 public interface IHotelDAO {
 
+	/**
+	 * Stores a hotel in to the DB
+	 * @param hotel the hotel you want to store
+	 * @return
+	 */
 	public Hotel storeHotel(Hotel hotel);
+
+	/**
+	 * Gets Hotel by ID from the DB
+	 * @param hotelID the ID from the hotel we want to search
+	 * @return
+	 */
 	public Hotel getHotel(String hotelID);
+
+	/**
+	 * Gets all the hotels from the DB
+	 * @return arraylist of hotels
+	 */
 	public ArrayList<Hotel> getHotels();
+
+	/**
+	 * Deletes a hotel by ID
+	 * @param hotelID
+	 * @return true if its been deleted, false if not
+	 */
 	public boolean deleteHotel(String hotelID);
+
+	/**
+	 * Clean all the DB
+	 */
 	public void cleanDB();
 }
