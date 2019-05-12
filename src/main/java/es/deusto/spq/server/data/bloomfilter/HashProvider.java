b2ed10ca6192e26c210323c705bf6aed0511ac3d
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2019-05-12 15:01:24 +0200

    Documentation and code cleaned

--- src/main/java/es/deusto/spq/server/data/bloomfilter/HashProvider.java
+++ src/main/java/es/deusto/spq/server/data/bloomfilter/HashProvider.java
@@ -8,64 +8,98 @@ import java.security.NoSuchAlgorithmException;
 
 import es.deusto.spq.server.logger.ServerLogger;
 
+/**A simple class to provide some hasing functions.
+ * 
+ * @author Iker
+ *
+ */
 public class HashProvider {
 
+	/**Returns the SHA-1 of the given object.
+	 * @param object the object to be hashed.
+	 * @return the SHA-1 hash.
+	 */
 	public static String sha1(Object object) {
-		MessageDigest messageDigest = null;
-		try {
-			messageDigest = MessageDigest.getInstance("SHA-1");
-			byte [] serializedObject = serialize(object);
-			return new String(messageDigest.digest(serializedObject));
-		} catch (NoSuchAlgorithmException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
-		} catch (IOException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
+		if(object != null) {
+			MessageDigest messageDigest = null;
+			try {
+				messageDigest = MessageDigest.getInstance("SHA-1");
+				byte [] serializedObject = serialize(object);
+				return new String(messageDigest.digest(serializedObject));
+			} catch (NoSuchAlgorithmException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			} catch (IOException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			}
 		}
 		return new String("");
 	}
 	
+	/**Returns the SHA-256 of the given object.
+	 * @param object the object to be hashed.
+	 * @return the SHA-256 hash.
+	 */
 	public static String sha256(Object object) {
-		MessageDigest messageDigest = null;
-		try {
-			messageDigest = MessageDigest.getInstance("SHA-256");
-			byte [] serializedObject = serialize(object);
-			return new String(messageDigest.digest(serializedObject));
-		} catch (NoSuchAlgorithmException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
-		} catch (IOException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
+		if(object != null) {
+			MessageDigest messageDigest = null;
+			try {
+				messageDigest = MessageDigest.getInstance("SHA-256");
+				byte [] serializedObject = serialize(object);
+				return new String(messageDigest.digest(serializedObject));
+			} catch (NoSuchAlgorithmException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			} catch (IOException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			}
 		}
 		return new String("");
 	}
 
+	/**Returns the SHA-384 of the given object.
+	 * @param object the object to be hashed.
+	 * @return the SHA-384 hash.
+	 */
 	public static String sha384(Object object) {
-		MessageDigest messageDigest = null;
-		try {
-			messageDigest = MessageDigest.getInstance("SHA-384");
-			byte [] serializedObject = serialize(object);
-			return new String(messageDigest.digest(serializedObject));
-		} catch (NoSuchAlgorithmException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
-		} catch (IOException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
+		if(object != null) {
+			MessageDigest messageDigest = null;
+			try {
+				messageDigest = MessageDigest.getInstance("SHA-384");
+				byte [] serializedObject = serialize(object);
+				return new String(messageDigest.digest(serializedObject));
+			} catch (NoSuchAlgorithmException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			} catch (IOException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			}
 		}
 		return new String("");
 	}
 	
+	/**Returns the SHA-512 of the given object.
+	 * @param object the object to be hashed.
+	 * @return the SHA-512 hash.
+	 */
 	public static String sha512(Object object) {
-		MessageDigest messageDigest = null;
-		try {
-			messageDigest = MessageDigest.getInstance("SHA-512");
-			byte [] serializedObject = serialize(object);
-			return new String(messageDigest.digest(serializedObject));
-		} catch (NoSuchAlgorithmException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
-		} catch (IOException e) {
-			ServerLogger.getLogger().warn(e.getMessage());
+		if(object != null) {
+			MessageDigest messageDigest = null;
+			try {
+				messageDigest = MessageDigest.getInstance("SHA-512");
+				byte [] serializedObject = serialize(object);
+				return new String(messageDigest.digest(serializedObject));
+			} catch (NoSuchAlgorithmException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			} catch (IOException e) {
+				ServerLogger.getLogger().warn(e.getMessage());
+			}
 		}
 		return new String("");
 	}
 	
+	/**Serializes the object into an array of bytes.
+	 * @param obj the object to be serialized.
+	 * @return an array of bytes - the serialized object.
+	 * @throws IOException launched while trying to serialize.
+	 */
 	private static byte[] serialize(Object obj) throws IOException {
 	    ByteArrayOutputStream out = new ByteArrayOutputStream();
 	    ObjectOutputStream os = new ObjectOutputStream(out);
--- src/main/java/es/deusto/spq/server/data/bloomfilter/SimpleBloomFilter.java
+++ src/main/java/es/deusto/spq/server/data/bloomfilter/SimpleBloomFilter.java
@@ -6,20 +6,40 @@ import org.apache.log4j.Logger;
 
 import es.deusto.spq.server.logger.ServerLogger;
 
+/**A simple implementation of a BloomFilter.
+ * <p>
+ * The functionality is as simple as {@code add}, {@code merge}, {@code contains} and {@code clear}.
+ *  
+ * @author Iker
+ *
+ * @param <T> the class this filter will work with.
+ */
 public class SimpleBloomFilter<T> {
 
+	/** The set bits of hashes. */
 	private BitSet hashes;
-	private int size = 1024 * 1024 * 8; // Default size: 1 MiB
-	private int numberHashFunctions = 3; // The number of hash functions used in the current filter
+	/** The size (in bytes) of the bloom filter (default value: 1MiB). */
+	private int size = 1024 * 1024 * 8;
+	/** The number of hash functions used in the current filter. */
+	private int numberHashFunctions = 3;
+	/** The maximum number of elements to be stored (-1 == no limit). */
 	private int maximumNumberElements = -1;
+	/** Number of elements that have been added. */
 	private int numberAddedElements = 0;
+	/** The logger to log to. */
 	private Logger log;
 	
+	/**Creates a new instance of the {@code SimpleBloomFilter}.
+	 * The default size is used to build the filter: 1MiB.
+	 */
 	public SimpleBloomFilter() {
 		hashes = new BitSet(size);
 		log = ServerLogger.getLogger();
 	}
 	
+	/**Creates a new instance of the {@code SimpleBloomFilter}.
+	 * @param maximumNumberElements the maximum number of elements the filter can support.
+	 */
 	public SimpleBloomFilter(int maximumNumberElements) {
 		if(maximumNumberElements > 0)
 			this.maximumNumberElements = maximumNumberElements;
@@ -27,6 +47,9 @@ public class SimpleBloomFilter<T> {
 		log = ServerLogger.getLogger();
 	}
 	
+	/**Adds the object to the filter.
+	 * @param object the object to be added.
+	 */
 	public void add(T object) {
 		// Adds the object to the filter either if there's no maximum number of elements or it has not been reached 
 		if(maximumNumberElements == -1 || 
@@ -44,25 +67,24 @@ public class SimpleBloomFilter<T> {
 		}
 	}
 	
+	/**Merges the given SimpleBloomFilter into the current one. Note that two filters must have the same size and 
+	 * use the same hash functions to be able to be merged.
+	 * @param toBeMerged the filter to be merged.
+	 * @throws IllegalArgumentException launched when {@code toBeMerged} is null or does not fulfill any of the 
+	 * requirements described above (same size and hash functions).
+	 */
 	public void merge(SimpleBloomFilter<T> toBeMerged) throws IllegalArgumentException {
 		if(toBeMerged == null || toBeMerged.getSize() != size || toBeMerged.getNumberHashFunctions() != numberHashFunctions)
 			throw new IllegalArgumentException("Filter's size and hash functions must be equal to be merged");
 		hashes.or(toBeMerged.getHashes());
 		log.info("Merged another SimpleBloomFilter");
 	}
-	
-	public int getSize() {
-		return size;
-	}
-	
-	public int getNumberHashFunctions() {
-		return numberHashFunctions;
-	}
-	
-	public BitSet getHashes() {
-		return hashes;
-	}
-	
+
+	/**Returns whether the object has been stored before in the filter. Note that there's chance of returning false positives. 
+	 * @param object the object to be checked.
+	 * @return {@code false} when the object has not been added before, and 
+	 * 			{@code true} when the object has been added (or a false positive).
+	 */
 	public boolean contains(T object) {
 		int hashCode = object.hashCode();
 		if(hashes.get(hashCode % size) || hashes.get((hashCode >> 16) % size) || hashes.get((hashCode >> 8) % size))
@@ -70,10 +92,33 @@ public class SimpleBloomFilter<T> {
 		return false;
 	}
 	
+	/**Clears the filter.
+	 */
 	public void clear() {
 		hashes.clear();
 		numberAddedElements = 0;
 		log.info("Filter cleared");
+	}
+
+	/**Returns the size of the filter.
+	 * @return the size in bytes.
+	 */
+	public int getSize() {
+		return size;
+	}
+	
+	/**Returns the number of hash functions that are being used in the current filter.
+	 * @return the number of hash functions.
+	 */
+	public int getNumberHashFunctions() {
+		return numberHashFunctions;
+	}
+	
+	/**Returns the hashes of the filter.
+	 * @return the BitSet of hashes.
+	 */
+	public BitSet getHashes() {
+		return hashes;
 	}
 	
 }
