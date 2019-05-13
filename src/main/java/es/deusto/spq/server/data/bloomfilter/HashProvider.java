1872600537f2e3886ae69fe32283cf5fccb8a704
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2019-05-13 21:52:48 +0200

    FIX HashProvider to avoid actions like padding

--- src/main/java/es/deusto/spq/server/data/bloomfilter/HashProvider.java
+++ src/main/java/es/deusto/spq/server/data/bloomfilter/HashProvider.java
@@ -14,25 +14,27 @@ import es.deusto.spq.server.logger.ServerLogger;
  *
  */
 public class HashProvider {
-
+	
 	/**Returns the SHA-1 of the given object.
 	 * @param object the object to be hashed.
 	 * @return the SHA-1 hash.
 	 */
 	public static String sha1(Object object) {
-		if(object != null) {
-			MessageDigest messageDigest = null;
-			try {
-				messageDigest = MessageDigest.getInstance("SHA-1");
-				byte [] serializedObject = toByteArray(object);
-				return new String(messageDigest.digest(serializedObject).toString());
-			} catch (NoSuchAlgorithmException e) {
-				ServerLogger.getLogger().warn("SHA-1 1: " + e.getMessage());
-			} catch (IOException e) {
-				ServerLogger.getLogger().warn("SHA-1 2: " + e.getMessage());
-			}
+		if(object == null)
+			return null;
+        MessageDigest md = null;
+        byte [] before = null;
+        byte [] hashInBytes = null;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			before = toByteArray(object);
+			hashInBytes = md.digest(before);
+		} catch (NoSuchAlgorithmException e) {
+			ServerLogger.getLogger().warn("SHA-1: " + e.getMessage());
+		} catch (IOException e) {
+			ServerLogger.getLogger().warn("SHA-1: " + e.getMessage());
 		}
-		return new String("");
+        return bytesToHex(hashInBytes);
 	}
 	
 	/**Returns the SHA-256 of the given object.
@@ -40,19 +42,21 @@ public class HashProvider {
 	 * @return the SHA-256 hash.
 	 */
 	public static String sha256(Object object) {
-		if(object != null) {
-			MessageDigest messageDigest = null;
-			try {
-				messageDigest = MessageDigest.getInstance("SHA-256");
-				byte [] serializedObject = toByteArray(object);
-				return new String(messageDigest.digest(serializedObject).toString());
-			} catch (NoSuchAlgorithmException e) {
-				ServerLogger.getLogger().warn("SHA-256 1: " + e.getMessage());
-			} catch (IOException e) {
-				ServerLogger.getLogger().warn("SHA-256 2: " + e.getMessage());
-			}
+		if(object == null)
+			return null;
+        MessageDigest md = null;
+        byte [] before = null;
+        byte [] hashInBytes = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			before = toByteArray(object);
+			hashInBytes = md.digest(before);
+		} catch (NoSuchAlgorithmException e) {
+			ServerLogger.getLogger().warn("SHA-256: " + e.getMessage());
+		} catch (IOException e) {
+			ServerLogger.getLogger().warn("SHA-256: " + e.getMessage());
 		}
-		return new String("");
+        return bytesToHex(hashInBytes);
 	}
 
 	/**Returns the SHA-384 of the given object.
@@ -60,19 +64,21 @@ public class HashProvider {
 	 * @return the SHA-384 hash.
 	 */
 	public static String sha384(Object object) {
-		if(object != null) {
-			MessageDigest messageDigest = null;
-			try {
-				messageDigest = MessageDigest.getInstance("SHA-384");
-				byte [] serializedObject = toByteArray(object);
-				return new String(messageDigest.digest(serializedObject).toString());
-			} catch (NoSuchAlgorithmException e) {
-				ServerLogger.getLogger().warn("SHA-384: " + e.getMessage());
-			} catch (IOException e) {
-				ServerLogger.getLogger().warn("SHA-384: " + e.getMessage());
-			}
+		if(object == null)
+			return null;
+		MessageDigest md = null;
+        byte [] before = null;
+        byte [] hashInBytes = null;
+		try {
+			md = MessageDigest.getInstance("SHA-384");
+			before = toByteArray(object);
+			hashInBytes = md.digest(before);
+		} catch (NoSuchAlgorithmException e) {
+			ServerLogger.getLogger().warn("SHA-384: " + e.getMessage());
+		} catch (IOException e) {
+			ServerLogger.getLogger().warn("SHA-384: " + e.getMessage());
 		}
-		return new String("");
+        return bytesToHex(hashInBytes);
 	}
 	
 	/**Returns the SHA-512 of the given object.
@@ -80,19 +86,21 @@ public class HashProvider {
 	 * @return the SHA-512 hash.
 	 */
 	public static String sha512(Object object) {
-		if(object != null) {
-			MessageDigest messageDigest = null;
-			try {
-				messageDigest = MessageDigest.getInstance("SHA-512");
-				byte [] serializedObject = toByteArray(object);
-				return new String(messageDigest.digest(serializedObject).toString());
-			} catch (NoSuchAlgorithmException e) {
-				ServerLogger.getLogger().warn("SHA-512: " + e.getMessage());
-			} catch (IOException e) {
-				ServerLogger.getLogger().warn("SHA-512: " + e.getMessage());
-			}
+		if(object == null)
+			return null;
+		MessageDigest md = null;
+        byte [] before = null;
+        byte [] hashInBytes = null;
+		try {
+			md = MessageDigest.getInstance("SHA-512");
+			before = toByteArray(object);
+			hashInBytes = md.digest(before);
+		} catch (NoSuchAlgorithmException e) {
+			ServerLogger.getLogger().warn("SHA-512: " + e.getMessage());
+		} catch (IOException e) {
+			ServerLogger.getLogger().warn("SHA-512: " + e.getMessage());
 		}
-		return new String("");
+        return bytesToHex(hashInBytes);
 	}
 	
 	/**Parses the object to an array of bytes.
@@ -121,4 +129,18 @@ public class HashProvider {
         return bytes;
     }
 	
+	/**Parses the hashed byte array to a human-readable hex string.
+	 * @param bytes the byte array to be parsed.
+	 * @return the parsed string.
+	 */
+	private static String bytesToHex(byte [] bytes) {
+		if(bytes == null)
+			return null;
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+	}
+
 }
