38d6fb68e31da87843e828bddcf15555b427f10f
Egoitz <egoes98@gmail.com>
2019-05-17 19:39:00 +0200

    Documentation-Localization
    Added some documentation and localization. Some visual enancements must be done.

--- src/main/java/es/deusto/spq/client/gui/views/reviews/ShowHotelReviewView.java
+++ src/main/java/es/deusto/spq/client/gui/views/reviews/ShowHotelReviewView.java
@@ -33,6 +33,11 @@ import java.util.List;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * View for showing the Reviews of the previously selected Hotel
+ * @author egoes
+ *
+ */
 public class ShowHotelReviewView extends View {
 
 	private JInternalFrame frame;
@@ -97,9 +102,10 @@ public class ShowHotelReviewView extends View {
 		frame.getContentPane().add(scrollPane);
 
 		editorPane = new JEditorPane();
+		editorPane.setEditable(false);
 		scrollPane.setViewportView(editorPane);
 		
-		lblTittle = new JLabel(getViewManager().getClient().getLocaleManager().getMessage("showReviews.tittle"));
+		lblTittle = new JLabel(getViewManager().getClient().getLocaleManager().getMessage("showReviews.tittle"), JLabel.TRAILING);
 		lblTittle.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblTittle.setBounds(191, 11, 85, 25);
 		frame.getContentPane().add(lblTittle);
@@ -114,26 +120,36 @@ public class ShowHotelReviewView extends View {
 		reviewsList = controller.retrieveReviews("");
 		averageScore = 0;
 
+		//Handle the reviews depending on the number of reviews the hotel has
 		if(reviewsList == null || reviewsList.isEmpty()) {
+			//There are no reviews so it writes a message
 			editorPane.setText("\n\n"+getViewManager().getClient().getLocaleManager().getMessage("showReviews.noReviews"));
 		}else if(reviewsList.size() == 1) {
+			//1 review only, displays the review 
 			editorPane.setText(reviewsList.get(0).getScore()+"-"+reviewsList.get(0).getOpinion());
 			averageScore = reviewsList.get(0).getScore();
 		}else {
+			//More than 1 review
 			String opinion;
 			String previousText;
 
 			opinion = reviewsList.get(0).getScore() +"-"+reviewsList.get(0).getOpinion();
 			averageScore = reviewsList.get(0).getScore();
+			//Gets all the reviews and display them on the EditorPane
 			for(int n = 1; n < reviewsList.size(); n++) {
 				averageScore += reviewsList.get(n).getScore();
 				previousText = editorPane.getText();
 				opinion = reviewsList.get(n).getScore() +"-"+ reviewsList.get(n).getOpinion();
 				editorPane.setText(previousText+"\n\n"+opinion);
 			}
+			//Calculate the average score of the hotel
 			averageScore = averageScore/reviewsList.size();
 		}
-		lblAverageScore.setText(getViewManager().getClient().getLocaleManager().getMessage("showReviews.averageScore")+averageScore);
+
+		//Displays the Average Score
+		lblAverageScore = new JLabel(getViewManager().getClient().getLocaleManager().getMessage("showReviews.averageScore")+" "+averageScore, JLabel.TRAILING);
+		lblAverageScore.setBounds(191, 47, 108, 14);
+		frame.getContentPane().add(lblAverageScore);
 		frame.setVisible(true);
 	}
 	
